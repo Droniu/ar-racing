2bff616f6aa8ee881e5bcb2e13e5e2b30dd2e3c3
Paul <pawelpelar@gmail.com>
2023-11-26 18:46:19 +0100

    Basic CarController

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,7 +6,6 @@ public class CarController : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
     public float turnSpeed = 100.0f;
-    public float maxSpeed = 10.0f;
 
     private Rigidbody rb;
 
@@ -17,20 +16,14 @@ public class CarController : MonoBehaviour
 
     void Update()
     {
-        // Sterowanie skrętem za pomocą akcelerometra
         Vector3 turn = new Vector3(0f, Input.acceleration.x * turnSpeed * Time.deltaTime, 0f);
         transform.Rotate(turn);
 
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
-        // Sprawdź, czy ekran jest dotykany
         if (Input.touchCount > 0)
         {
-            // Dodaj siłę do przodu, tylko jeśli prędkość jest mniejsza niż maksymalna
-            if (rb.velocity.magnitude < maxSpeed)
-            {
-                rb.AddForce(transform.forward * moveSpeed);
-            }
+            rb.AddForce(transform.forward * moveSpeed);
         }
     }
 }
--- Assets/Scripts/SurfaceObjectPlacer.cs
+++ Assets/Scripts/SurfaceObjectPlacer.cs
@@ -13,28 +13,19 @@ public class SurfaceObjectPlacer : MonoBehaviour
 
     void Update()
     {
-        // Sprawdź, czy użytkownik dotknął ekranu
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
 
-            // Wykonaj raycast, gdy użytkownik dotknie ekranu
             if (touch.phase == TouchPhase.Began)
             {
                 if (raycastManager.Raycast(touch.position, raycastHits, TrackableType.PlaneWithinPolygon))
                 {
                     Pose hitPose = raycastHits[0].pose;
 
-                    // Jeśli jeszcze nie umieszczono obiektu, umieść go. W przeciwnym razie przesuń istniejący obiekt.
                     if (placedObject == null)
                     {
                         placedObject = Instantiate(objectToPlacePrefab, hitPose.position, hitPose.rotation);
-                    }
-                    else
-                    {
-                        
-                        //placedObject.transform.position = hitPose.position;
-                        //placedObject.transform.rotation = hitPose.rotation;
                     }
                 }
             }
